73736b14c13d918162c02a4dad50ca921548ee2b
Catarino Rodrigues <contabilnc@hotmail.com>
2021-11-21 15:04:05 -0300

    Criação do Projeto Cidade HUB

---
+++ cidade_hub/src/main/java/br/com/cidade_hub/CidadeHubApplication.java
@@ -0,0 +1,13 @@
+package br.com.cidade_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidadeHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidadeHubApplication.class, args);
+	}
+
+}
